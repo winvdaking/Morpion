14427f3cbfcae1cae0504181db5f2838e4546c37
Dorian <pro.dorianlopez@gmail.com>
2024-11-05 10:59:19 +0100

    feat: ajouter des tests avec Jest et configurer SonarCloud

---
+++ tests/Game.test.js
@@ -0,0 +1,56 @@
+import Game from '../src/class/Game.js';
+import Board from '../src/class/Board.js';
+
+describe('Board', () => {
+    let board;
+
+    beforeEach(() => {
+        board = new Board();
+    });
+
+    test('doit initialiser le tableau vide', () => {
+        expect(board.board).toEqual([
+            [' ', ' ', ' '],
+            [' ', ' ', ' '],
+            [' ', ' ', ' ']
+        ]);
+    });
+
+    test('doit placer une X', () => {
+        board.placeMark(0, 0, 'X');
+        expect(board.board[0][0]).toBe('X');
+    });
+
+    test('ne doit pas placer une X sur une place occuper', () => {
+        board.placeMark(0, 0, 'X');
+        expect(board.placeMark(0, 0, 'O')).toBe(false);
+    });
+
+    test('doit détecter un gagnant', () => {
+        board.placeMark(0, 0, 'X');
+        board.placeMark(0, 1, 'X');
+        board.placeMark(0, 2, 'X');
+        expect(board.checkWinner('X')).toBe(true);
+    });
+
+    test('doit détecter un tableau remplit', () => {
+        board.board = [
+            ['X', 'O', 'X'],
+            ['X', 'O', 'X'],
+            ['O', 'X', 'O']
+        ];
+        expect(board.isFull()).toBe(true);
+    });
+});
+
+describe('Game', () => {
+    let game;
+
+    beforeEach(() => {
+        game = new Game();
+    });
+
+    test('should initialize with a random current player', () => {
+        expect(['O', 'X']).toContain(game.currentPlayer);
+    });
+});
